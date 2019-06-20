47ffb0b46a61debf471f13ca42734eb43b141424
Zander Ackerman <zandera1@vt.edu>
2019-06-20 16:37:03 -0500

    Made some stuff
    Completely untested, just saving my progress.

--- OSMParser.py
+++ OSMParser.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw
 
 #Straight Outta an Internet Tutorial
 #WARNING! This function uses a module that is not secure against maliciously constructed data!
@@ -15,23 +16,85 @@ def parse_XML(xmlfile):
 	
 	
 	nodeList = []
+	nodeLocList = []
 	for item in root.findall('node'):
 		nodeList.append(item)
-	osmData.append(nodeList)
+		#finds nodes that have descriptive tags denoting they are map features
+		if (item.find('tag') != None):
+			nodeLocList.append(item)
 	
 	wayList = []
 	for item in root.findall('way'):
 		wayList.append(item)
-	osmData.append(wayList)
 	
+	bounds = tree.find('bounds')
+	min = (bounds.get('minlat'), bounds.get('minlon'))
+	max = (bounds.get('maxlat'), bounds.get('maxlon'))
+	
+	
+	idDict = {}
+	for item in nodeList:
+		cord = (item.get('lat'), item.get('lon'))
+		id = item.get('id')
+		#picked an arbitrary scaling value
+		idDict[id] = cord_to_pix(min, max, cord, 10000000)
+		
+	typeDict = {}
+	numberOfTypes = 0
+	for item in wayList:
+		if
+
 	return osmData
+	
+def image_gen(idDict, objList, objectType, bounds):
+	image = Image.new('RGBA', (bounds[0], bounds[1]), 'white')
+	
+	for item in objList:
+		nodes = item.findall('nd')
+		points = []
+		for nd in nodes
+			#picked an arbitrary road width
+			points.append(nd.get('ref'), width = '10')
+		
+		draw = ImageDraw.Draw(image)
+		draw.line(points)
+	
+	#this should get the name of the type of objects in the object list
+	#assumes the object list consists of objects of the same type
+	listType = (objList[0].find('tag').items())[0][0]
+	
+	image.save(objectType + '.png')
+
+#maps coordinates in a bounded space to pixel locations in an image.
+def cord_to_pix(min, max, cord, scale):
+	
+	minlat = min[0]
+	minlon = min[1]
+	maxlat = max[0]
+	maxlon = max[1]
+	
+	offsetlat = maxlat - minlat
+	offsetlon = maxlon - minlon
+	
+	offsetCordlat = cord[0] - minlat
+	offsetCordlon = cord[1] - minlon
+	
+	scalelat = offsetCordlat / offsetlat
+	scalelon = offsetCordlon / offsetlon
+	
+	pixX = round(scale * scalelat)
+	pixY = round((scale * (offsetCordlon / offsetCordlat)) * scalelon)
+	
+	pix = (pixX, pixY)
+	
+	return pix
 	
 #handle what to do when the script is called
 def main():
 	parseOut = parse_XML("map.osm.xml")
 	
 	#output all the ways
-	for item in parseOut[1]:
+	for item in parseOut[2]:
 		print(item.attrib)
 		print("\n")
 	
